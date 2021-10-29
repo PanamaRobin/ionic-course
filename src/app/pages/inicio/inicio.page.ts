65951310f1fb5d493ad97a1e2d565412579056bd
Jorge Herrera <jorherrera@bgeneral.com>
2021-10-29 06:10:56 -0500

    [Feature]: ion alert added

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -6,31 +6,31 @@ interface Componente {
   redirectTo: string;
 }
 
-
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
   styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage implements OnInit {
-
   componentes: Componente[] = [
     {
       icon: 'american-football-outline',
       name: 'Action Sheet',
-      redirectTo: '/action-sheet'
+      redirectTo: '/action-sheet',
     },
     {
       icon: 'alert-circle-outline',
       name: 'Alert',
-      redirectTo: '/alert'
-    }
+      redirectTo: '/alert',
+    },
+    {
+      icon: 'beaker-outline',
+      name: 'Avatar',
+      redirectTo: '/avatar',
+    },
   ];
 
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
